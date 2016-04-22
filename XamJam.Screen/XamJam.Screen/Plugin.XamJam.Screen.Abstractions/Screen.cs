5507f0d29849b36ec1e4d7e2f52fcb4da6c47a5b
Jason HandUber <jhanduber@gmail.com>
2016-04-22 08:39:08 -0400

    adding icon and docs

--- XamJam.Screen/XamJam.Screen/Plugin.XamJam.Screen.Abstractions/Screen.cs
+++ XamJam.Screen/XamJam.Screen/Plugin.XamJam.Screen.Abstractions/Screen.cs
@@ -10,6 +10,10 @@ namespace Plugin.XamJam.Screen.Abstractions
   {
         /// <summary>
         /// Returns this platforms total screen size. The size available to any app at any point in time will not exceed this size.
+        /// The following references were useful: 
+        /// http://03cd0a8.netsolhost.com/wordpress/?p=90
+        /// https://github.com/XLabs/Xamarin-Forms-Labs/wiki/Device
+        /// https://github.com/aritchie/deviceinfo
         /// </summary>
         ScreenSize Size { get; }
   }
