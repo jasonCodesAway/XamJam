49ee6aef7b54679d65d6a0dbba5bc246007c77fd
czanbaka <czanbaka@gmail.com>
2016-04-29 10:10:28 -0400

    fixing those damn yellow triangles

--- XamJam.Nav/Tab/SexyTabbedItemView.xaml.cs
+++ XamJam.Nav/Tab/SexyTabbedItemView.xaml.cs
@@ -8,6 +8,9 @@ using Xamarin.Forms;
 
 namespace XamJam.Nav.Tab
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public partial class SexyTabbedItemView : ContentView
     {
         public SexyTabbedItemView(TabDestination viewModel)
