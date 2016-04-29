61c9970b9ffe35aead41ea1d16af5b137e6a61da
Jason HandUber <jhanduber@gmail.com>
2016-04-28 21:18:11 -0400

    Committing XamJam.Util

---
+++ XamJam.Util/ColorExtensionMethods.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+using Xamarin.Forms;
+
+namespace XamJam.Util
+{
+    public static class ColorExtensionMethods
+    {
+        public static string ToPlainHex(this Color color)
+        {
+            return color.R.ToString("X2") + color.G.ToString("X2") + color.B.ToString("X2");
+        }
+
+        public static Color FromPlainHex(this string plainHex)
+        {
+            var argb = uint.Parse(plainHex, NumberStyles.HexNumber);
+            return Color.FromUint(argb);
+        }
+    }
+}
---
+++ XamJam.Util/ListExtensionMethods.cs
@@ -0,0 +1,33 @@
+﻿#region
+
+using System;
+using System.Collections.Generic;
+
+#endregion
+
+namespace XamJam.Util
+{
+    public static class ListExtensionMethods
+    {
+        private static readonly Random rng = new Random();
+
+        public static T Random<T>(this List<T> list)
+        {
+            return list[rng.Next(list.Count)];
+        }
+
+        public static T NewRandom<T>(this List<T> list, T oldT)
+        {
+            if (list.Count == 1 && list.Contains(oldT))
+            {
+                throw new ArgumentException("Cannot generate a new random value, there is only 1 value in the list");
+            }
+            var newT = list[rng.Next(list.Count)];
+            while (EqualityComparer<T>.Default.Equals(newT, oldT))
+            {
+                newT = list[rng.Next(list.Count)];
+            }
+            return newT;
+        }
+    }
+}
