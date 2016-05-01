010e9bf24e3fc35ea492797c19dcca9440e182ed
Jason HandUber <jhanduber@gmail.com>
2016-05-01 14:35:28 -0400

    works on android now, sort of

--- XamJam.Ratings/XamJam.RatingsSample.Droid/MainActivity.cs
+++ XamJam.Ratings/XamJam.RatingsSample.Droid/MainActivity.cs
@@ -10,7 +10,7 @@ using XamSvg.XamForms.Droid;
 
 namespace XamJam.RatingsSample.Droid
 {
-    [Activity(Label = "XamJam.RatingsSample", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "GestureSample", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
     {
         protected override void OnCreate(Bundle bundle)
