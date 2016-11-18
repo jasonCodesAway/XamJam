4bf712991d46a9d5eec24b2beb63d3bea8e5acce
jasonCodesAway <jhanduber@live.com>
2016-11-18 14:10:25 -0500

    minor

---
+++ cycleLocal.bat
@@ -0,0 +1,17 @@
+REM grab the project name from the current directory...yay batch files
+for %%* in (.) do set rawProjectName=%%~nx*
+call set projectNameWithUnderscores=%%rawProjectName:.=_%%%
+echo Building %projectNameWithUnderscores%
+
+cd ..
+set msbuild.exe=
+for /D %%D in (%SYSTEMROOT%\Microsoft.NET\Framework\v4*) do set msbuild.exe=%%D\MSBuild.exe
+if not defined msbuild.exe echo error: can't find MSBuild.exe & goto :eof
+if not exist "%msbuild.exe%" echo error: %msbuild.exe%: not found & goto :eof
+%msbuild.exe% XamJam.sln /t:%projectNameWithUnderscores% /p:Configuration=Release
+
+del ..\LocalNuget\*%rawProjectName%*.nupkg
+cd %rawProjectName%
+nuget pack Plugin.%rawProjectName%.nuspec
+set "nugetName=XamJam.Plugins.%rawProjectName%"
+move /Y *%rawProjectName%*.nupkg ..\..\LocalNuget
