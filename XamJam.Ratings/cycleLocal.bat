9ee4bce235ae7b24c33f3d8063d69989b1f765ac
czanbaka <czanbaka@gmail.com>
2016-11-19 10:21:43 -0500

    Removing XamJam.Rating

--- XamJam.Ratings/cycleLocal.bat
+++
@@ -1,3 +0,0 @@
-del ..\..\LocalNuget\Xam.Plugins.XamJam.Ratings.*.nupkg
-nuget pack Plugin.XamJam.Ratings.nuspec
-move /Y Xam.Plugins.XamJam.Ratings.*.nupkg ..\..\LocalNuget
--- XamJam.Ratings/cycleRemote.bat
+++
@@ -1,4 +0,0 @@
-rem must build release first
-del Xam.Plugins.XamJam.Ratings.*.nupkg
-nuget pack Plugin.XamJam.Ratings.nuspec
-nuget push Xam.Plugins.XamJam.Ratings.*.nupkg
